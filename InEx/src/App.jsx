0e0464e03ab4471582830230c5533e135e8907ec
SHUUAA <143815744+SHUUAA@users.noreply.github.com>
2025-06-12 17:35:46 +0800

    initial

---
+++ InEx/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-red-500">It's working</div>
+    </>
+  );
+}
+
+export default App;
